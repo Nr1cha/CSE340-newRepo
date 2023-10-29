63cd2a74d1825add28ae0227edd511ee454f27e6
Nick Richards <vrm.richards@gmail.com>
2023-10-28 21:10:32 -0600

    added the password hash code and added new package

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,63 +1,85 @@
-const utilities = require("../utilities") // this is my connection to the index. ask brooke
-const accountModel = require("../models/account-model")
+const utilities = require("../utilities"); // this is my connection to the index. ask brooke
+const accountModel = require("../models/account-model");
+const bcrypt = require("bcryptjs");
 
 /* ****************************************
  *  Deliver login view
  * *************************************** */
 async function buildLogin(req, res, next) {
-    let nav = await utilities.getNav();
-    const loginView = await utilities.buildLogin()
-    req.flash("notice", "This is a flash message.")
-    res.render("account/login", {
-        title: "Login",
-        nav,
-        loginView,
-    });
+  let nav = await utilities.getNav();
+  const loginView = await utilities.buildLogin();
+  req.flash("notice", "This is a flash message.");
+  res.render("account/login", {
+    title: "Login",
+    nav,
+    loginView,
+  });
 }
 
 /* ****************************************
-*  Deliver registration view
-* *************************************** */
+ *  Deliver registration view
+ * *************************************** */
 async function buildRegister(req, res, next) {
-    let nav = await utilities.getNav()
-    res.render("account/register", {
-        title: "Register",
-        nav,
-        errors: null,
-    })
+  let nav = await utilities.getNav();
+  res.render("account/register", {
+    title: "Register",
+    nav,
+    errors: null,
+  });
 }
 
 /*****************************************
-*  Process Registration
-* ****************************************/
+ *  Process Registration
+ * ****************************************/
 async function registerAccount(req, res) {
-    let nav = await utilities.getNav()
-    const { account_firstname, account_lastname, account_email, account_password
-    } = req.body
+  let nav = await utilities.getNav();
+  const {
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password,
+  } = req.body;
 
-    const regResult = await accountModel.registerAccount(
-        account_firstname,
-        account_lastname,
-        account_email,
-        account_password
-    )
+  // Hash the password before storing
+  let hashedPassword;
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10);
+  } catch (error) {
+    req.flash(
+      "notice",
+      "Sorry, there was an error processing the registration."
+    );
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    });
+  }
 
-    if (regResult) {
-        req.flash(
-            "notice",
-            `Congratulations, you\'re registered ${account_firstname}. Please log in.`
-        )
-        res.status(201).render("account/login", {
-            title: "Login",
-            nav,
-        })
-    } else {
-        req.flash("notice", "Sorry, the registration failed.")
-        res.status(501).render("account/register", {
-            title: "Registration",
-            nav,
-        })
-    }
+  const regResult = await accountModel.registerAccount(
+    account_firstname,
+    account_lastname,
+    account_email,
+    hashedPassword
+  );
+
+  if (regResult) {
+    req.flash(
+      "notice",
+      `Congratulations, you\'re registered ${account_firstname}. Please log in.`
+    );
+    res.status(201).render("account/login", {
+      title: "Login",
+      nav,
+    });
+  } else {
+    req.flash("notice", "Sorry, the registration failed.");
+    res.status(501).render("account/register", {
+      title: "Registration",
+      nav,
+    });
+  }
 }
 
 module.exports = { buildLogin, buildRegister, registerAccount };
